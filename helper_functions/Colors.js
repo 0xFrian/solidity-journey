31b1feb25de18235c6826cf8b9ba3496172b6d0f
0xFrian <brianfong22@gmail.com>
2022-04-07 23:16:16 -0700

    Added module for colored-text console logging

---
+++ helper_functions/Colors.js
@@ -0,0 +1,22 @@
+const COLORS = {
+    "blue" : "\x1b[36m",
+    "yellow" : "\x1b[93m",
+    "purple" : "\x1b[95m",
+    "green" : "\x1b[32m",
+    "gray" : "\x1b[90m",
+    "reset" : "\x1b[0m"
+}
+const CHECK = "  " + COLORS["green"] + "✓" + COLORS["reset"] + " "; 
+
+function highlight(msg, color) {
+    highlighted_msg = COLORS[color] + msg + COLORS["reset"];
+    return highlighted_msg;
+}
+
+const EXPORT = {
+    "COLORS" : COLORS,
+    "CHECK" : CHECK,
+    "highlight" : highlight
+};
+
+module.exports = { COLORS, CHECK, highlight };
