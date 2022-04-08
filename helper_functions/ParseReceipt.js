5a628f71c6bd9a24fd1284b789559f0fa9750779
0xFrian <brianfong22@gmail.com>
2022-04-08 02:22:47 -0700

    Added logs functionality

--- helper_functions/ParseReceipt.js
+++ helper_functions/ParseReceipt.js
@@ -1,13 +1,23 @@
 const { highlight } = require("../helper_functions/Colors");
 
 const parseReceipt = (name, receipt) => {
+    let events = "";
+    if (receipt.events.length > 0) {
+        receipt.events.forEach(element => events += element.event)  ;      
+    }
+
+    let logs = "";
+    if (receipt.logs.length > 0) {
+        receipt.logs.forEach(element => logs += parseInt(element.data, 16));
+    }
+    
     console.log(`Transaction Receipt for ${highlight(name, "yellow")} contract: `);
     console.log(` * To: ${highlight(receipt.to, "purple")}, From: ${highlight(receipt.from, "purple")}`);
     console.log(` * Contract Address: ${highlight(receipt.contractAddress, "purple")}`);
     console.log(` * Transaction Index: ${highlight(receipt.transactionIndex, "blue")}, Transaction Hash: ${highlight(receipt.transactionHash, "purple")}`);
     console.log(` * Block Number: ${highlight(receipt.blockNumber, "blue")}, Block Hash: ${highlight(receipt.blockHash, "purple")}`);
     console.log(` * Status: ${highlight(receipt.status, "blue")}, Gas Used: ${highlight(receipt.gasUsed, "blue")}`);
-    console.log(` * Logs: ${highlight(receipt.logs, "yellow")}, Events: ${highlight(receipt.events, "yellow")}`);
+    console.log(` * Logs: ${highlight(logs, "yellow")} Events: ${highlight(events, "yellow")}`);
 }
 
 exports.parseReceipt = parseReceipt;
